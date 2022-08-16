efe7734aac2c5ca2eeaff2416efdc3cafcd8438a
Chris Fanucchi <admin@knightskeep.com>
2022-08-16 15:42:09 -0700

    Login and question function added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 'use strict';
+document.querySelector('.login').addEventListener('click', function () {
+  let loginName = prompt('Please enter your first name to login:');
+  if (loginName !== null && loginName !== '') {
+    alert(
+      `Greetings, ${loginName}! \n\nYou will be asked a series of yes or no questions, to see how much you know about me. Good luck!`
+    );
+    let answer1 = askQuestion('Answer yes or no:', 'Yes');
+  } else {
+    alert('Please enter a valid login name to continue.');
+  }
+});
+
+const askQuestion = function (message, correctAnswer) {
+  let goodAnswer = false;
+  while (!goodAnswer) {
+    let answer = prompt(message);
+    if (answer != null) {
+      answer = answer.toLowerCase();
+      if (answer === 'yes' || answer === 'y') answer = 'Yes';
+      if (answer === 'no' || answer === 'n') answer = 'No';
+      if (answer === 'Yes' || answer === 'No') {
+        console.log(
+          `The answer to '${message}' is ${correctAnswer}. You were ${
+            answer === correctAnswer ? 'CORRECT' : 'INCORRECT'
+          }!`
+        );
+        return answer;
+      }
+    }
+    alert('Please provide a valid entry of yes, no, y or n.');
+  }
+};
